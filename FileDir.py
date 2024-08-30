87a7d4af72ebb2e89b6f14a19d762ba6993c5b06
Dustin McDonnell <89492902+Dustin-McDonnell@users.noreply.github.com>
2024-08-30 11:35:52 -0400

    Roadblocked, need to do some research

--- FileDir.py
+++ FileDir.py
@@ -2,11 +2,20 @@ import os
 import shutil
 import tkinter
 from tkinter import filedialog
+from tkinter import ttk
+
+
+def user_interface():
+    root = tkinter.Tk()
+    frm = ttk.Frame(root, padding=10)
+    frm.grid()
+    ttk.Button(frm, text="Source", command=get_dir).grid(column=1,row=0)
+    ttk.Button(frm, text="JPG Destination", command=get_dir).grid(column=1, row=1)
+    root.protocol('WM_DELETE_WINDOW')
+    root.mainloop()
 
 
 def get_dir():
-    root = tkinter.Tk()
-    root.withdraw()
     dir_name = filedialog.askdirectory()
     return dir_name
 
@@ -22,8 +31,9 @@ def move_files(dir_list, path):
 
 
 def run():
-    dir_name = get_dir()
-    dest_name = get_dir()
-    dir_list = list_dir(dir_name)
-    move_files(dir_list,dest_name)
+    #dir_name = get_dir()
+    #dest_name = get_dir()
+    #dir_list = list_dir(dir_name)
+    #move_files(dir_list,dest_name)
+    user_interface()
 
